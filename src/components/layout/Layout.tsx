6fd900cc41394b8fa80e8903c873ba3cd6b644ba
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2026-07-27 14:02:11 +0300

    Add MapPin icon to layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -665,6 +665,7 @@ function Footer() {
         Alsoor St, Burj Jassem, Floor 12, Kuwait
       </p>
     </div>
+  <MapPin className="w-4 h-4 mt-1 flex-shrink-0" />
 
     <div>
       <p className="text-white/70 text-xs font-semibold uppercase tracking-wide mb-1">
