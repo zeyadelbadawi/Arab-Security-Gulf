566a1a51aaea8ebbfad4363e659dc590989ab4e2
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2026-07-27 14:05:21 +0300

    Add duplicate MapPin component in Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -668,6 +668,8 @@ function Footer() {
   <MapPin className="w-4 h-4 mt-1 flex-shrink-0" />
 
     <div>
+        <MapPin className="w-4 h-4 mt-1 flex-shrink-0" />
+
       <p className="text-white/70 text-xs font-semibold uppercase tracking-wide mb-1">
         Branch & Service Center
       </p>
